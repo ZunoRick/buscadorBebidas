957c1cb26f9ae335b7dc0942a26fc0808999062e
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-05-24 22:40:16 -0500

    Creando un context y Obteniendo Categorias de la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,9 @@
 import { Button, Form, Row, Col  } from 'react-bootstrap'
+import useCategorias from '../hooks/useCategorias'
 
 const Formulario = () => {
+  const { categorias } = useCategorias()
+
   return (
     <Form>
       <Row>
@@ -26,8 +29,26 @@ const Formulario = () => {
               name='categoria'
             >
               <option>- Selecciona Categoría -</option>
+              { categorias.map( categoria => (
+                <option
+                  key={ categoria.strCategory }
+                  value={ categoria.strCategory }
+                > { categoria.strCategory }
+                </option>
+              )) }
             </Form.Select>
           </Form.Group>
+        </Col>
+      </Row>
+
+      <Row className='justify-content-end'>
+        <Col md={3}>
+          <Button 
+            variant='danger'
+            className='text-uppercase w-100'
+          >
+            Buscar Bebidas
+          </Button>
         </Col>
       </Row>
     </Form>
