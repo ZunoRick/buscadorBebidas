daedc9d461a7c907700bab4d12530b65d8cb4f20
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-05-25 01:25:33 -0500

    Mostrando receta en un modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Formulario from './components/Formulario'
 import ListadoBebidas from './components/ListadoBebidas'
+import ModalBebida from './components/ModalBebida'
 import { CategoriasProvider } from './context/CategoriasProvider'
 import { BebidasProvider } from './context/BebidasProvider'
 
@@ -16,6 +17,8 @@ function App() {
           <Formulario />
 
           <ListadoBebidas />
+
+          <ModalBebida />
         </Container>
       </BebidasProvider>
     </CategoriasProvider>
--- src/context/BebidasProvider.jsx
+++ src/context/BebidasProvider.jsx
@@ -5,6 +5,10 @@ const BebidasContext = createContext()
 
 const BebidasProvider = ({children}) =>{
   const [bebidas, setBebidas] = useState([])
+  const [modal, setModal] = useState(false)
+  const [bebidaId, setBebidaId] = useState(null)
+  const [receta, setReceta] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   const consultarBebida = async (datos) =>{
     try {
@@ -16,11 +20,42 @@ const BebidasProvider = ({children}) =>{
     }
   }
 
+  useEffect(() => {
+    setCargando(true)
+    const obtenerReceta = async ()=>{
+      if(!bebidaId) return
+
+      try {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${bebidaId}`
+        const { data } = await axios(url)
+        setReceta(data.drinks[0]);
+      } catch (error) {
+        console.log(error);
+      } finally{
+        setCargando(false)
+      }
+    }
+    obtenerReceta()
+  }, [bebidaId])
+
+  const handleBebidaIdClick = id =>{
+    setBebidaId(id)
+  }
+
+  const handleModalClick = () =>{
+    setModal(!modal)
+  }
+
   return(
     <BebidasContext.Provider
       value={{
         consultarBebida,
-        bebidas
+        bebidas,
+        handleModalClick,
+        modal,
+        handleBebidaIdClick,
+        receta,
+        cargando
       }}
     >
       {children}
